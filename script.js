b7113201a947b88f81a0cc6233b02ac82a20e80a
Student <student@Admins-MacBook-Air-2.local>
2018-03-17 12:32:56 -0700

    update

--- script.js
+++ script.js
@@ -40,10 +40,15 @@ var alienBulletY;
  * variables by calling resetGame().
  */
  function setup() {
-   canvas = creatCanvas(500,400);
+   canvas = createCanvas(500,400);
    background(20,30,40);
    gameScreen = select("#game-screen");
    canvas.parent(gameScreen);
+   shipColor = "#00ff00";
+   shipDiameter = 80;
+   shipX = width / 2;
+   shipY = height - (shipDiameter/2);
+   shipSpeed = 8;
  }
 
 /*
@@ -66,14 +71,34 @@ var alienBulletY;
  * This function animates the ship, alien, and both kinds of bullets, but only
  * if the game is running.
  */
-
+ function draw(){
+   background(20,30,40)
+   drawShip();
+ }
 
 /*
  * drawShip()
  * This function draws the player's ship. It also controls the ship's
  * x value by checking if the player is holding down the left or right keys.
  */
+ function drawShip() {
+   if(keyIsDown(LEFT_ARROW) && shipX > (shipDiameter/2)){
+     shipX -= shipSpeed;
+   }
+   if(keyIsDown(RIGHT_ARROW) && shipX < width - shipDiameter/2){
+     shipX += shipSpeed;
+   }/*
+   if(keyIsDown(UP_ARROW)){
+     shipY -= shipSpeed;
+   }
+/  if(keyIsDown(DOWN_ARROW)){
+     shipY += shipSpeed;
+   }
+   */
+   fill(shipColor);
+   ellipse(shipX, shipY, shipDiameter, shipDiameter);
 
+ }
 
 /*
  * keyPressed()
